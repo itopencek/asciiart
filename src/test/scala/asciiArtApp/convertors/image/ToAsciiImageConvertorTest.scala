41beb05b86db2d4dd5b423d0cbe02f5563a5ae30
Ivan Havasi <ivohavasi@gmail.com>
2021-12-11 18:51:09 +0100

    ToAsciiImageConvertorTest updated, still not working

--- src/test/scala/asciiArtApp/convertors/image/ToAsciiImageConvertorTest.scala
+++ src/test/scala/asciiArtApp/convertors/image/ToAsciiImageConvertorTest.scala
@@ -8,14 +8,17 @@ import asciiArtApp.models.pixel.char.CharPixel
 import org.scalatest.FunSuite
 
 import java.io.File
+import scala.io.Source
 
 class ToAsciiImageConvertorTest extends FunSuite {
   def imageConvertor(item: RgbImage): CharImage = new ToAsciiImageConvertor().convert(item)
   def load(source: String): RgbImage = new FileRgbImageLoader().load(source)
-  val imageColors: RgbImage = load(new File("src/test/resources/colors.bmp").getAbsolutePath)
-  val imageOop: RgbImage = load(new File("src/test/resources/bi-oop.jpg").getAbsolutePath)
-  val imageCowboy: RgbImage = load(new File("src/test/resources/image-cowboy.png").getAbsolutePath)
-  val imageUsa: RgbImage = load(new File("src/test/resources/statue-of-liberty.jpg").getAbsolutePath)
+
+  val baseUrl = "src/test/resources"
+  val imageColors: RgbImage = load(new File(baseUrl + "/colors.bmp").getAbsolutePath)
+  val imageOop: RgbImage = load(new File(baseUrl + "/bi-oop.jpg").getAbsolutePath)
+  val imageCowboy: RgbImage = load(new File(baseUrl + "/image-cowboy.png").getAbsolutePath)
+  val imageUsa: RgbImage = load(new File(baseUrl + "/statue-of-liberty.jpg").getAbsolutePath)
 
   test("Test basic conversion") {
     val result = imageConvertor(imageColors)
@@ -24,10 +27,16 @@ class ToAsciiImageConvertorTest extends FunSuite {
 
     assert(result == asciiImage)
   }
+
   // TODO
   test("Cowboy") {
-    val result = imageConvertor(imageCowboy)
+    /*val result = imageConvertor(imageUsa)
     val visitor = new ImageTextRenderVisitor
-    print(visitor.visitAsciiImage(result))
+
+    val resultString = visitor.visitAsciiImage(result)
+    val fileContents = Source.fromFile(baseUrl + "/converted/statue-of-liberty.txt").getLines.mkString
+
+    assert(resultString == fileContents)*/
+
   }
 }
