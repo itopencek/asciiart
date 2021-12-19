1858d3b613abd71d649464132cba7c8c594630d9
Ivan Havasi <ivohavasi@gmail.com>
2021-12-19 13:50:47 +0100

    Added tests for convertors

--- src/test/scala/asciiArtApp/convertors/image/ToAsciiImageConvertorTest.scala
+++ src/test/scala/asciiArtApp/convertors/image/ToAsciiImageConvertorTest.scala
@@ -1,26 +1,21 @@
 package asciiArtApp.convertors.image
 
-import asciiArtApp.loaders.image.file.specific.{BitmapLoader, JpgLoader, PngLoader}
-import asciiArtApp.models.grid.pixel.GreyscaleGrid
+import asciiArtApp.loaders.image.file.specific.BitmapLoader
+import asciiArtApp.models.grid.pixel.{GreyscaleGrid, RgbGrid}
 import asciiArtApp.models.image.grid.{GreyscaleImage, RgbImage}
 import asciiArtApp.models.pixel.char.GreyscalePixel
+import asciiArtApp.models.pixel.rgb.RGBPixel
 import org.scalatest.FunSuite
 
 import java.io.File
 
 class ToAsciiImageConvertorTest extends FunSuite {
   def imageConvertor(item: RgbImage): GreyscaleImage = new ToAsciiImageConvertor().convert(item)
-  // all image loaders
-  def loadJPG(source: String): RgbImage = new JpgLoader(source).load()
-  def loadPNG(source: String): RgbImage = new PngLoader(source).load()
   def loadBMP(source: String): RgbImage = new BitmapLoader(source).load()
 
   // images for testing
   val baseUrl = "src/test/resources"
   val imageColors: RgbImage = loadBMP(new File(baseUrl + "/colors.bmp").getAbsolutePath)
-  val imageOop: RgbImage = loadJPG(new File(baseUrl + "/bi-oop.jpg").getAbsolutePath)
-  val imageCowboy: RgbImage = loadPNG(new File(baseUrl + "/image-cowboy.png").getAbsolutePath)
-  val imageUsa: RgbImage = loadJPG(new File(baseUrl + "/statue-of-liberty.jpg").getAbsolutePath)
 
   test("Test basic conversion") {
     val result = imageConvertor(imageColors)
@@ -31,15 +26,40 @@ class ToAsciiImageConvertorTest extends FunSuite {
     assert(result == asciiImage)
   }
 
-  // TODO
-  test("Cowboy") {
-    /*val result = imageConvertor(imageUsa)
-    val visitor = new ImageTextRenderVisitor
+  test("Test black image") {
+    val array = Array[RGBPixel](RGBPixel(0, 0, 0), RGBPixel(0, 0, 0), RGBPixel(0, 0, 0), RGBPixel(0, 0, 0),
+      RGBPixel(0, 0, 0), RGBPixel(0, 0, 0))
+    val image = RgbImage(3, 2, RgbGrid(2, array))
 
-    val resultString = visitor.visitAsciiImage(result)
-    val fileContents = Source.fromFile(baseUrl + "/converted/statue-of-liberty.txt").getLines.mkString
+    val converted = imageConvertor(image)
 
-    assert(resultString == fileContents)*/
+    val expectedArray = Array[GreyscalePixel](GreyscalePixel("$".head, 0), GreyscalePixel("$".head, 0),
+      GreyscalePixel("$".head, 0), GreyscalePixel("$".head, 0), GreyscalePixel("$".head, 0),
+      GreyscalePixel("$".head, 0))
 
+    assert(converted == GreyscaleImage(3, 2, GreyscaleGrid(3, expectedArray)))
+  }
+
+  test("Test white image") {
+    val array = Array[RGBPixel](RGBPixel(255, 255, 255), RGBPixel(255, 255, 255), RGBPixel(255, 255, 255),
+      RGBPixel(255, 255, 255), RGBPixel(255, 255, 255), RGBPixel(255, 255, 255))
+    val image = RgbImage(3, 2, RgbGrid(3, array))
+
+    val converted = imageConvertor(image)
+
+    val expectedArray = Array[GreyscalePixel](GreyscalePixel(" ".head, 255), GreyscalePixel(" ".head, 255),
+      GreyscalePixel(" ".head, 255), GreyscalePixel(" ".head, 255), GreyscalePixel(" ".head, 255),
+      GreyscalePixel(" ".head, 255))
+
+    assert(converted == GreyscaleImage(3, 2, GreyscaleGrid(3, expectedArray)))
+  }
+
+  test("Test empty image") {
+    val image = RgbImage(0, 0, RgbGrid(0, List[RGBPixel]()))
+    val convertedImage = GreyscaleImage(0, 0, GreyscaleGrid(0, List[GreyscalePixel]()))
+
+    val converted = imageConvertor(image)
+
+    assert(converted == convertedImage)
   }
 }
